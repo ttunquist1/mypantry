f0ee743f7e03526f5031ca181bf2e263dadd0f73
bustedchickn <talmage.kay@gmail.com>
2026-03-04 19:52:12 -0700

    Update build.gradle.kts

--- my_pantry/android/app/build.gradle.kts
+++ my_pantry/android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.my_pantry"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
